0352968a0100fff6ca66b40691e2702cac9a0a41
jyou044 <jyou044@uottawa.ca>
2022-08-27 23:45:10 -0400

    Added env file and gitignore

--- bot.py
+++ bot.py
@@ -1,8 +1,28 @@
+'''
+Code written by Jason You.
+This is a simple python discord bot I made to send 
+commands within my discord server.
+'''
+
+# Imports
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
-client = commands.Bot(command_prefix="$")
+# Initialize intents
+intents = discord.Intents.default()
+intents.message_content = True
 
+# Load env file
+load_dotenv()
+# Get environment variable for the access token
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+
+# Initialize client (bot command is "$")
+client = commands.Bot(command_prefix="$", intents=intents)
+
+# Events that occur based on command given
 @client.event
 async def on_ready():
     print("Bot is ready")
@@ -22,4 +42,4 @@ async def oops(ctx):
     
 
 
-client.run("")
+client.run(ACCESS_TOKEN)
